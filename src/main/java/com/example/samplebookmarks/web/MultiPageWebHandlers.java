81f83e79bd2b428ccf34c486f7c7074351f1c853
jeremychone <jeremy.chone@gmail.com>
2013-02-10 10:29:28 -0800

    . remove uncessary @WebParam

--- src/main/java/com/example/samplebookmarks/web/MultiPageWebHandlers.java
+++ src/main/java/com/example/samplebookmarks/web/MultiPageWebHandlers.java
@@ -6,7 +6,6 @@ import javax.inject.Inject;
 
 import com.britesnow.snow.web.handler.annotation.WebModelHandler;
 import com.britesnow.snow.web.param.annotation.WebModel;
-import com.britesnow.snow.web.param.annotation.WebParam;
 import com.britesnow.snow.web.param.annotation.WebUser;
 import com.example.samplebookmarks.dao.ItemDao;
 import com.example.samplebookmarks.entity.User;
@@ -28,10 +27,9 @@ public class MultiPageWebHandlers {
      * 
      * @param m (@WebModel) will be injected by Snow, and it is the WebModel for the page 
      * @param user (@WebUser) will be injected by Snow, and represents the Authenticate user. Null if nobody authenticated.
-     * @param id (@WebParam) injected by Snow, with the default system resolveLong param resolver (will return null if not a number or absent)
      */
     @WebModelHandler(startsWith="/bookmarks")
-    public void bookmarksPage(@WebModel Map m, @WebUser User user, @WebParam("id")Long id){
+    public void bookmarksPage(@WebModel Map m, @WebUser User user){
         if (user != null){
             m.put("items", itemDao.getItemsForUser(user.getId()));
         }
